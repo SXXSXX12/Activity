7650e3c7c9f59aef500277846dcc9fb1009ee6c7
SXXSXX <SXXSXX@100.125.23.20>
2017-09-15 07:03:39 +0700

    reportq     15/09/2017

--- report_act1.php
+++ report_act1.php
@@ -11,21 +11,21 @@
             <div class="alert alert-success col-lg-12">
                 <?php
                 include_once 'funtion/funcDateThai.php';
-                $act_id=$_GET['act_id'];
-                if(empty($_GET['year'])){
+                $act_id = $_GET['act_id'];
+                if (empty($_GET['year'])) {
                     $code = '';
                 } else {
-                   $year=$_GET['year'];
-                   $code = "AND SUBSTR(h.std_code,1,2)='$year'"; 
-}            
+                    $year = $_GET['year'];
+                    $code = "AND SUBSTR(h.std_code,1,2)='$year'";
+                }
                 $strsql = "SELECT * FROM activity where act_id=$act_id";
                 $result = mysqli_query($conn, $strsql);
-                $rs= mysqli_fetch_array($result);
-                echo "ชื่อกิจกรรม :".$rs['act_name']."<br>";
-                echo "จำนวนกลุ่มเป้าหมาย :".$rs['act_number']."<br>";
-                echo "วันที่เริ่ม :".DateThai1($rs['act_datestart'])."<br>";
-                echo "วันที่สิ้นสุด :".DateThai1($rs['act_dateend'])."<br>";
-                
+                $rs = mysqli_fetch_array($result);
+                echo "ชื่อกิจกรรม :" . $rs['act_name'] . "<br>";
+                echo "จำนวนกลุ่มเป้าหมาย :" . $rs['act_number'] . "<br>";
+                echo "วันที่เริ่ม :" . DateThai1($rs['act_datestart']) . "<br>";
+                echo "วันที่สิ้นสุด :" . DateThai1($rs['act_dateend']) . "<br>";
+
                 $sqlstr = "SELECT s.std_code,CONCAT(s.fname,' ',s.lname) AS fullname
                 FROM history_act h
                 INNER JOIN activity a ON a.act_id=h.act_id
@@ -33,9 +33,24 @@
                 WHERE h.status_regis=2 
                 $code 
                 AND a.act_id='$act_id'";
+                $sumq = "SELECT 
+SUM(CASE sex
+WHEN '1' THEN 1
+WHEN '2' THEN 0
+ELSE NULL END) AS men,
+SUM(CASE sex
+WHEN '1' THEN 0
+WHEN '2' THEN 1
+ELSE NULL END) AS women,
+AVG(q1) AS q1,AVG(q2) AS q2,AVG(q3) AS q3,AVG(q4) AS q4,AVG(q5) AS q5,
+AVG(q6) AS q6,AVG(q7) AS q7,AVG(q8) AS q8,AVG(q9) AS q9,AVG(q10) AS q10
+FROM question
+WHERE act_id=$act_id";
+                $result3 = mysqli_query($conn, $sumq);
+                $rs1 = mysqli_fetch_array($result3);
                 $result2 = mysqli_query($conn, $sqlstr);
                 $num_row = mysqli_num_rows($result2);
-                echo "มีนักศึกษาเข้าร่วมกิจกรรมจำนวน ".$num_row." คน คิดเป็น ". round(($num_row*100)/$rs['act_number'],3)."%";
+                echo "มีนักศึกษาเข้าร่วมกิจกรรมจำนวน " . $num_row . " คน คิดเป็น " . round(($num_row * 100) / $rs['act_number'], 3) . "%";
                 ?>
                 <table width='100%' align='center'>
                     <tr>
@@ -45,7 +60,6 @@
                         <td align='center'>รายละเอียดการเข้ากิจกรรม</td>
                     </tr>
                     <?php
-                    
                     $row_no = 0;
                     while ($rs = mysqli_fetch_array($result2)) {
                         $row_no++;
@@ -58,6 +72,69 @@
                         </tr>
                     <?php } ?>
 
+
+                </table>
+            </div>
+            <div class="alert alert-success col-lg-12">
+                รายงานผลแบบประเมิน
+                <table width='100%'>
+                    <tr bgcolor="pink">
+
+                        <th align='center' rowspan="2">รายการ</th>
+                        <th align='center' colspan="3">ผลการประเมิน</th>
+                        <th align='center' rowspan="2">ผลการประเมิน</th>
+                    </tr> 
+                    <tr bgcolor="pink">
+                        <th align='center'>X</th>
+                        <th align='center'>S.D</th>
+                        <th align='center'>ค่าร้อยละ</th>
+                    </tr>
+                    <tr>
+                        <td align='left'colspan="5"><h4><b>ด้านความพึงพอใจในการเข้าร่วมโครงการ</b></h4></td>
+                    </tr>
+                    <tr>
+                        <td align='left'>1 การประชาสัมพันธ์ข่าวสาร</td>
+                        <td align='center'><?= round($rs1['q1'],2); ?></td>
+                    </tr>
+                    <tr>
+                        <td align='left'>2 สถานที่ในการจัดโครงการ</td>
+                        <td align='center'><?= round($rs1['q2'],2); ?></td>
+                    </tr>  
+                    <tr>
+                        <td align='left'>3ระยะเวลาในการดำเนินโครงการ</td>
+                        <td align='center'><?= round($rs1['q3'],2); ?></td>
+                    </tr>  
+                    <tr>
+                        <td align='left'>4 การลงทะเบียน</td>
+                        <td align='center'><?= round($rs1['q4'],2); ?></td>
+                    </tr> 
+                    <tr>
+                        <td align='left'>5 การจัดบริการอาหาร/อาหารว่าง</td>
+                        <td align='center'><?= round($rs1['q5'],2); ?></td>
+                    </tr> 
+                    <tr>
+                        <td align='left' colspan="5"><h4><b>ด้านการนำความรู้ที่ได้ไปใช้ประโยชน์</b></h4></td>
+                    </tr> 
+                    <tr>
+                        <td align='left'>6 สามารถนำความรู้ที่ได้กลับไปประยุกต์ใช้งานได้จริง</td>
+                        <td align='center'><?= round($rs1['q6'],2); ?></td>
+                    </tr> 
+                    <tr>
+                        <td align='left'>7 สามารถกลับไปพัฒนาต่อยอดความรู้เดิมที่มีอยู่ได้</td>
+                        <td align='center'><?= round($rs1['q7'],2); ?></td>
+                    </tr> 
+                    <tr>
+                        <td align='left'>8 มีความมั่นใจและสามารถนำความรู้ที่ได้รับไปใช้ได้</td>
+                        <td align='center'><?= round($rs1['q8'],2); ?></td>
+                    </tr>
+                    <tr>
+                        <td align='left'>9 ผู้เข้าร่วมโครงการมีความรู้เพิ่มมากขึ้น</td>
+                        <td align='center'><?= round($rs1['q9'],2); ?></td>
+                    </tr> 
+                    <tr>
+                        <td align='left'>10 สามารถนำความรู้ที่ได้รับไปใช้งานได้จริง</td>
+                        <td align='center'><?= round($rs1['q10'],2); ?></td>
+                    </tr> 
 
                 </table>
             </div>
